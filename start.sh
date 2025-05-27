b1bdea94aad92b5d7bc44b5350a2c1d6c115ea9e
Promise Ibediogwu Ekele <pro9jaeduinfo@gmail.com>
2025-05-27 22:27:55 +0100

    start

--- start.sh
+++ start.sh
@@ -5,3 +5,10 @@ python download_model.py
 
 echo "Starting your app..."
 python app.py  # Replace with uvicorn main:app --host 0.0.0.0 --port 10000 if needed
+
+
+echo "Starting FastAPI..."
+uvicorn app:app #--host 0.0.0.0 --port 8000 &
+
+echo "Starting Streamlit..."
+streamlit run frontend/frontend.py #--server.port 8501 --server.address 0.0.0.0
